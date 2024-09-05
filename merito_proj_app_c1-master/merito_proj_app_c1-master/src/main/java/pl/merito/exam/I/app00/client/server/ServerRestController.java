58be814e432d6b64e168d7ad9b631d754f9b3107
barcinx <gruba.impra.xd@gmail.com>
2024-09-05 11:42:09 +0200

    ttps://github.com/barcinx/zaliczenieServer.git

---
+++ merito_proj_app_c1-master/merito_proj_app_c1-master/src/main/java/pl/merito/exam/I/app00/client/server/ServerRestController.java
@@ -0,0 +1,29 @@
+package pl.merito.exam.I.app00.client.server;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerRestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring is here!";
+    }
+
+    @GetMapping("/persons")
+    public List<Person> person() {
+        Person person = new Person();
+        person.setFirstName("FirstName");
+        return List.of(person);
+    }
+
+    @PostMapping("/person")
+    public Person addPerson(@RequestBody Person person) {
+        System.out.println(String.format("Person [%s] added ", person.toString()));
+        return person;
+    }
+}
